fa86d823b92e21b40d32b6976fc7779fc9d34338
UltralyticsAssistant <web@ultralytics.com>
2026-06-05 10:40:00 +0000

    Auto-format by https://ultralytics.com/actions

--- HCSC/README.md
+++ HCSC/README.md
@@ -1,22 +1,25 @@
 # HCSC: Hierarchical Contrastive Selective Coding
-This repository provides a PyTorch implementation and model weights for HCSC (**H**ierarchical **C**ontrastive **S**elective **C**oding), whose details are in [this paper](https://arxiv.org/pdf/2202.00455.pdf) (accepted by CVPR 2022). 
+
+This repository provides a PyTorch implementation and model weights for HCSC (**H**ierarchical **C**ontrastive **S**elective **C**oding), whose details are in [this paper](https://arxiv.org/pdf/2202.00455.pdf) (accepted by CVPR 2022).
 
 <p align="center">
   <img src="docs/framework.png" /> 
 </p>
 
-HCSC is an effective and efficient method to pre-train image encoders in a *self-supervised* fashion. 
+HCSC is an effective and efficient method to pre-train image encoders in a _self-supervised_ fashion.
 In general, this method seeks to learn **image representations with hierarchical semantic structures.**
-It utilizes *hierarchical K-means* to derive hierarchical prototypes, and these prototypes represent the hierarchical semantics underlying the data.
-On such basis, we perform *Instance-wise and Prototypical Contrastive Selective Coding* to inject the information within hierarchical prototypes into image representations.
-HCSC has achieved SOTA performance on the self-supervised pre-training of CNNs (*e.g.*, ResNet-50), and we will further study its potential on pre-training Vision Transformers. 
+It utilizes _hierarchical K-means_ to derive hierarchical prototypes, and these prototypes represent the hierarchical semantics underlying the data.
+On such basis, we perform _Instance-wise and Prototypical Contrastive Selective Coding_ to inject the information within hierarchical prototypes into image representations.
+HCSC has achieved SOTA performance on the self-supervised pre-training of CNNs (_e.g._, ResNet-50), and we will further study its potential on pre-training Vision Transformers.
 
 ### Roadmap
+
 - [2022/02/01] The initial release! We release all source code for pre-training and downstream evaluation. We release three pre-trained ResNet-50 models: 200 epochs (single-crop), 200 epochs (multi-crop) and 400 epochs (single-crop, batch size: 256).
 - [2022/03/03] More checkpoints released! We update the 400 epochs single-crop model (with 71.0 linear classification accuracy, 64.1 KNN accuracy) and add two new pre-trained ResNet-50 models: 400 epochs (multi-crop, batch size: 256) and 800 epochs (single-crop, batch size: 256).
 - [2022/03/22] 800 epochs multi-crop model (batch size: 256) released! This pre-trained model achieves 74.2 linear classification accuracy and 70.6 KNN accuracy.
 
 ### TODO
+
 - [x] Finish the pre-training of 400 epochs ResNet-50 models (multi-crop) and release.
 - [x] Finish the pre-training of 800 epochs ResNet-50 models (single- & multi-crop) and release.
 - [ ] Support Vision Transformer backbones.
@@ -24,38 +27,41 @@ HCSC has achieved SOTA performance on the self-supervised pre-training of CNNs (
 
 ## Model Zoo
 
-We will continually release our pre-trained HCSC model weights and corresponding training configs. The current finished ones are as follows:   
+We will continually release our pre-trained HCSC model weights and corresponding training configs. The current finished ones are as follows:
 
-| Backbone | Method | Crop | Epoch | Batch size | Lincls top-1 Acc. | KNN top-1 Acc. | url | config |
-|---------------------|-----------------|---------------------|--------------------|--------------------|----------------------|----------------------|-------------------|-------------------|
-| ResNet-50 | HCSC | Single | 200 | 256 | 69.2 | 60.7 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_200eps.pth) | [config](configs/single_crop_200eps_bs256.yaml) |
-| ResNet-50 | HCSC | Multi | 200 | 256 | 73.3 | 66.6 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_200eps.pth) | [config](configs/multi_crop_200eps_bs256.yaml) |
-| ResNet-50 | HCSC | Single | 400 | 256 | 71.0 | 64.1 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_400eps.pth) | [config](configs/single_crop_400eps_bs256.yaml) |
-| ResNet-50 | HCSC | Multi | 400 | 256 | 74.1 | 69.9 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_400eps.pth) | [config](configs/multi_crop_400eps_bs256.yaml) |
-| ResNet-50 | HCSC | Single | 800 | 256 | 72.0 | 64.5 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_800eps.pth) | [config](configs/single_crop_800eps_bs256.yaml) |
-| ResNet-50 | HCSC | Multi | 800 | 256 | 74.2 | 70.6 | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_800eps.pth) | [config](configs/multi_crop_800eps_bs256.yaml) |
+| Backbone  | Method | Crop   | Epoch | Batch size | Lincls top-1 Acc. | KNN top-1 Acc. | url                                                                                  | config                                          |
+| --------- | ------ | ------ | ----- | ---------- | ----------------- | -------------- | ------------------------------------------------------------------------------------ | ----------------------------------------------- |
+| ResNet-50 | HCSC   | Single | 200   | 256        | 69.2              | 60.7           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_200eps.pth)           | [config](configs/single_crop_200eps_bs256.yaml) |
+| ResNet-50 | HCSC   | Multi  | 200   | 256        | 73.3              | 66.6           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_200eps.pth) | [config](configs/multi_crop_200eps_bs256.yaml)  |
+| ResNet-50 | HCSC   | Single | 400   | 256        | 71.0              | 64.1           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_400eps.pth)           | [config](configs/single_crop_400eps_bs256.yaml) |
+| ResNet-50 | HCSC   | Multi  | 400   | 256        | 74.1              | 69.9           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_400eps.pth) | [config](configs/multi_crop_400eps_bs256.yaml)  |
+| ResNet-50 | HCSC   | Single | 800   | 256        | 72.0              | 64.5           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_800eps.pth)           | [config](configs/single_crop_800eps_bs256.yaml) |
+| ResNet-50 | HCSC   | Multi  | 800   | 256        | 74.2              | 70.6           | [model](https://hcscpretrained.s3.us-east-2.amazonaws.com/hcsc_multicrop_800eps.pth) | [config](configs/multi_crop_800eps_bs256.yaml)  |
 
 ## Installation
 
 Use following command to install dependencies (python3.7 with pip installed):
+
 ```
 pip3 install -r requirement.txt
 ```
 
 If having trouble installing PyTorch, follow the original guidance (https://pytorch.org/).
-Notably, the code is tested with ```cudatoolkit version 10.2```.
+Notably, the code is tested with `cudatoolkit version 10.2`.
 
 ## Pre-training on ImageNet
 
 Download [ImageNet](https://image-net.org/challenges/LSVRC/2012/) dataset under [ImageNet Folder]. Go to the path "[ImageNet Folder]/val" and use [this script](https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh) to build sub-folders.
 
 To train single-crop HCSC on 8 Tesla-V100-32GB GPUs for 200 epochs, run:
+
 ```
 python3 -m torch.distributed.launch --master_port [your port] --nproc_per_node=8 \
 pretrain.py [your ImageNet Folder]
 ```
 
 To train multi-crop HCSC on 8 Tesla-V100-32GB GPUs for 200 epochs, run:
+
 ```
 python3 -m torch.distributed.launch --master_port [your port] --nproc_per_node=8 \
 pretrain.py --multicrop [your ImageNet Folder]
@@ -66,13 +72,15 @@ pretrain.py --multicrop [your ImageNet Folder]
 ### <a name="lincls"></a> Evaluation: Linear Classification on ImageNet
 
 With a pre-trained model, to train a supervised linear classifier with all available GPUs, run:
+
 ```
 python3 eval_lincls_imagenet.py --data [your ImageNet Folder] \
 --dist-url tcp://localhost:10001 --world-size 1 --rank 0 \
 --pretrained [your pre-trained model (example:out.pth)]
 ```
 
-*Note*: for single-gpu evaluation, turn off the `multiprocessing-distributed`. For example, run:
+_Note_: for single-gpu evaluation, turn off the `multiprocessing-distributed`. For example, run:
+
 ```
 python3 eval_lincls_imagenet.py --data [your ImageNet Folder] \
 --dist-url tcp://localhost:10001 --world-size 1 --rank 0 \
@@ -83,6 +91,7 @@ python3 eval_lincls_imagenet.py --data [your ImageNet Folder] \
 ### Evaluation: KNN Evaluation on ImageNet
 
 To reproduce the KNN evaluation results with a pre-trained model using a single GPU, run:
+
 ```
 python3 -m torch.distributed.launch --master_port [your port] --nproc_per_node=1 eval_knn.py \
 --checkpoint_key state_dict \
@@ -95,6 +104,7 @@ python3 -m torch.distributed.launch --master_port [your port] --nproc_per_node=1
 To fine-tune a pre-trained model with 1% or 10% ImageNet labels with 8 Tesla-V100-32GB GPUs, run:
 
 #### 1% of labels:
+
 ```
 python3 -m torch.distributed.launch --nproc_per_node 8 --master_port [your port] eval_semisup.py \
 --labels_perc 1 \
@@ -103,6 +113,7 @@ python3 -m torch.distributed.launch --nproc_per_node 8 --master_port [your port]
 ```
 
 #### 10% of labels:
+
 ```
 python3 -m torch.distributed.launch --nproc_per_node 8 --master_port [your port] eval_semisup.py \
 --labels_perc 10 \
@@ -110,7 +121,8 @@ python3 -m torch.distributed.launch --nproc_per_node 8 --master_port [your port]
 [your ImageNet Folder]
 ```
 
-*Note*: for single-gpu evaluation, start the script without `torch.distributed.launch`. Remember to modify the `--batch_size` because it is the batch-size-per-gpu. Example:
+_Note_: for single-gpu evaluation, start the script without `torch.distributed.launch`. Remember to modify the `--batch_size` because it is the batch-size-per-gpu. Example:
+
 ```
 python3 eval_semisup.py --labels_perc 1 \
 --batch_size 256 --pretrained [your pretrained weights] \
@@ -124,8 +136,9 @@ python3 eval_semisup.py --labels_perc 1 \
 ##### 1. Download the [VOC](http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar) dataset.
 
 ##### 2. Finetune and evaluate on PASCAL VOC (with a single GPU):
+
 ```
-cd voc_cls/ 
+cd voc_cls/
 python3 main.py --data [your voc data folder] \
 --pretrained [your pretrained weights]
 ```
@@ -135,13 +148,14 @@ python3 main.py --data [your voc data folder] \
 ##### 1. Download the [Places205](http://places.csail.mit.edu/user/index.php) dataset (resized 256x256 version)
 
 ##### 2. Linear Classification on Places205 (with all available GPUs):
+
 ```
 python3 eval_lincls_places.py --data [your places205 data folder] \
 --data-url tcp://localhost:10001 \
 --pretrained [your pretrained weights]
 ```
 
-*Note*: for single-gpu evaluation, see [instruction above](#lincls).
+_Note_: for single-gpu evaluation, see [instruction above](#lincls).
 
 ### Evaluation: Transfer Learning - Object Detection on VOC / COCO
 
@@ -150,6 +164,7 @@ python3 eval_lincls_places.py --data [your places205 data folder] \
 #### 2. Install [detectron2](https://github.com/facebookresearch/detectron2/blob/main/INSTALL.md).
 
 #### 3. Convert a pre-trained model to the format of detectron2:
+
 ```
 cd detection
 python3 convert-pretrain-to-detectron2.py [your pretrained weight] out.pkl
@@ -158,6 +173,7 @@ python3 convert-pretrain-to-detectron2.py [your pretrained weight] out.pkl
 #### 4. Train on PASCAL VOC/COCO:
 
 ##### Finetune and evaluate on VOC (with 8 Tesla-V100-32GB GPUs):
+
 ```
 cd detection
 python3 train_net.py --config-file ./configs/pascal_voc_R_50_C4_24k_hcsc.yaml \
@@ -165,6 +181,7 @@ python3 train_net.py --config-file ./configs/pascal_voc_R_50_C4_24k_hcsc.yaml \
 ```
 
 ##### Finetune and evaluate on COCO (with 8 Tesla-V100-32GB GPUs):
+
 ```
 cd detection
 python3 train_net.py --config-file ./configs/coco_R_50_C4_2x_hcsc.yaml \
@@ -174,6 +191,7 @@ python3 train_net.py --config-file ./configs/coco_R_50_C4_2x_hcsc.yaml \
 ### Evaluation: Clustering Evaluation on ImageNet
 
 To reproduce the clustering evaluation results with a pre-trained model using all available GPUs, run:
+
 ```
 python3 eval_clustering.py --dist-url tcp://localhost:10001 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
@@ -182,7 +200,7 @@ python3 eval_clustering.py --dist-url tcp://localhost:10001 \
 [your ImageNet Folder]
 ```
 
-In the experiments of our paper, we set ```--num-cluster``` as 25000 and 1000.
+In the experiments of our paper, we set `--num-cluster` as 25000 and 1000.
 
 ## License
 
@@ -191,6 +209,7 @@ This repository is released under the MIT license as in the [LICENSE](LICENSE) f
 ## Citation
 
 If you find this repository useful, please kindly consider citing the following paper:
+
 ```
 @article{guo2022hcsc,
   title={HCSC: Hierarchical Contrastive Selective Coding},
--- ultralytics/nn/newmodules/GCDB.py
+++ ultralytics/nn/newmodules/GCDB.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.init as init
-import torch.nn.functional as F
-from ultralytics.nn.modules import C3
-class LayerNormFunction(torch.autograd.Function):
 
+from ultralytics.nn.modules import C3
+
+
+class LayerNormFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, weight, bias, eps):
         ctx.eps = eps
-        N, C, H, W = x.size()
+        _N, C, _H, _W = x.size()
         mu = x.mean(1, keepdim=True)
         var = (x - mu).pow(2).mean(1, keepdim=True)
         y = (x - mu) / (var + eps).sqrt()
@@ -20,48 +20,57 @@ class LayerNormFunction(torch.autograd.Function):
     def backward(ctx, grad_output):
         eps = ctx.eps
 
-        N, C, H, W = grad_output.size()
+        _N, C, _H, _W = grad_output.size()
         y, var, weight = ctx.saved_variables
         g = grad_output * weight.view(1, C, 1, 1)
         mean_g = g.mean(dim=1, keepdim=True)
 
         mean_gy = (g * y).mean(dim=1, keepdim=True)
-        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
-        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
-            dim=0), None
-  
-class LayerNorm2d(nn.Module):
+        gx = 1.0 / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return (
+            gx,
+            (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0),
+            grad_output.sum(dim=3).sum(dim=2).sum(dim=0),
+            None,
+        )
 
+
+class LayerNorm2d(nn.Module):
     def __init__(self, channels, eps=1e-6):
-        super(LayerNorm2d, self).__init__()
-        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
-        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        super().__init__()
+        self.register_parameter("weight", nn.Parameter(torch.ones(channels)))
+        self.register_parameter("bias", nn.Parameter(torch.zeros(channels)))
         self.eps = eps
 
     def forward(self, x):
         return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
-    
+
 
 class SimpleGate(nn.Module):
     def forward(self, x):
         x1, x2 = x.chunk(2, dim=1)
         return x1 * x2
+
+
 ############GC#####################
 class DWConv(nn.Module):  # 定义一个深度可分离卷积类
     def __init__(self, dim=768):  # 初始化函数，dim是输入通道数，默认值为768
-        super(DWConv, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, bias=True, groups=dim)  # 定义一个深度可分离卷积层
+        super().__init__()
+        self.dwconv = nn.Conv2d(
+            dim, dim, kernel_size=3, stride=1, padding=1, bias=True, groups=dim
+        )  # 定义一个深度可分离卷积层
 
     def forward(self, x, H, W):  # 前向传播函数，x是输入，H和W是目标的高度和宽度
-        B, N, C = x.shape  # 获取输入的batch大小B，特征数N，通道数C
+        B, _N, C = x.shape  # 获取输入的batch大小B，特征数N，通道数C
         x = x.transpose(1, 2).view(B, C, H, W).contiguous()  # 将输入的维度重排为适合卷积操作的4D张量
         x = self.dwconv(x)  # 进行深度可分离卷积
         x = x.flatten(2).transpose(1, 2)  # 将卷积结果展平，恢复回原来的维度
         return x
-        
+
+
 class GatedConvolutionalLinearModule(nn.Module):  # 定义一个门控卷积线性模块类
     def __init__(self, in_channels, drop=0.1):  # 初始化函数，in_channels是输入通道数，drop是Dropout的比例
-        super(GatedConvolutionalLinearModule, self).__init__()
+        super().__init__()
 
         hidden_channels = int(2 * in_channels)  # 定义隐藏通道数为2倍的输入通道数
         self.fc1 = nn.Linear(in_channels, hidden_channels * 2)  # 定义一个全连接层fc1，将输入映射到隐藏空间
@@ -80,50 +89,96 @@ class GatedConvolutionalLinearModule(nn.Module):  # 定义一个门控卷积线�
         x = self.drop(x)  # 使用Dropout
         x = x.reshape(b, h, w, c).permute(0, 3, 1, 2)  # 将输出张量重排回4D张量
         return x
-###########################################################################################    
-class Branch(nn.Module):
-    '''
-    Branch that lasts lonly the dilated convolutions
-    '''
-    def __init__(self, c, DW_Expand, dilation = 1):
-        super().__init__()
-        self.dw_channel = DW_Expand * c 
-        
-        self.branch = nn.Sequential(
-                       nn.Conv2d(in_channels=self.dw_channel, out_channels=self.dw_channel, kernel_size=3, padding=dilation, stride=1, groups=self.dw_channel,
-                                            bias=True, dilation = dilation) # the dconv
-        )
-    def forward(self, input):
-        return self.branch(input)    
-class GCDB(nn.Module):
-    '''
-    Change this block using Branch
-    '''
-    
-    def __init__(self, c, DW_Expand=2, FFN_Expand=2, dilations = [1], extra_depth_wise = False):
-        super().__init__()
-        #we define the 2 branches
-        self.dw_channel = DW_Expand * c 
 
-        self.conv1 = nn.Conv2d(in_channels=c, out_channels=self.dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dilation = 1)
-        self.extra_conv = nn.Conv2d(self.dw_channel, self.dw_channel, kernel_size=3, padding=1, stride=1, groups=c, bias=True, dilation=1) if extra_depth_wise else nn.Identity() #optional extra dw
+
+###########################################################################################
+class Branch(nn.Module):
+    """Branch that lasts lonly the dilated convolutions."""
+
+    def __init__(self, c, DW_Expand, dilation=1):
+        super().__init__()
+        self.dw_channel = DW_Expand * c
+
+        self.branch = nn.Sequential(
+            nn.Conv2d(
+                in_channels=self.dw_channel,
+                out_channels=self.dw_channel,
+                kernel_size=3,
+                padding=dilation,
+                stride=1,
+                groups=self.dw_channel,
+                bias=True,
+                dilation=dilation,
+            )  # the dconv
+        )
+
+    def forward(self, input):
+        return self.branch(input)
+
+
+class GCDB(nn.Module):
+    """Change this block using Branch."""
+
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, dilations=[1], extra_depth_wise=False):
+        super().__init__()
+        # we define the 2 branches
+        self.dw_channel = DW_Expand * c
+
+        self.conv1 = nn.Conv2d(
+            in_channels=c,
+            out_channels=self.dw_channel,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+            dilation=1,
+        )
+        self.extra_conv = (
+            nn.Conv2d(
+                self.dw_channel, self.dw_channel, kernel_size=3, padding=1, stride=1, groups=c, bias=True, dilation=1
+            )
+            if extra_depth_wise
+            else nn.Identity()
+        )  # optional extra dw
         self.branches = nn.ModuleList()
         for dilation in dilations:
-            self.branches.append(Branch(self.dw_channel, DW_Expand = 1, dilation = dilation))
-            
+            self.branches.append(Branch(self.dw_channel, DW_Expand=1, dilation=dilation))
+
         assert len(dilations) == len(self.branches)
-        self.dw_channel = DW_Expand * c 
+        self.dw_channel = DW_Expand * c
         self.sca = nn.Sequential(
-                       nn.AdaptiveAvgPool2d(1),
-                       nn.Conv2d(in_channels=self.dw_channel // 2, out_channels=self.dw_channel // 2, kernel_size=1, padding=0, stride=1,
-                       groups=1, bias=True, dilation = 1),  
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(
+                in_channels=self.dw_channel // 2,
+                out_channels=self.dw_channel // 2,
+                kernel_size=1,
+                padding=0,
+                stride=1,
+                groups=1,
+                bias=True,
+                dilation=1,
+            ),
         )
         self.sg1 = GatedConvolutionalLinearModule(self.dw_channel)
         self.sg2 = GatedConvolutionalLinearModule(FFN_Expand * c)
-        self.conv3 = nn.Conv2d(in_channels=self.dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dilation = 1)
+        self.conv3 = nn.Conv2d(
+            in_channels=self.dw_channel // 2,
+            out_channels=c,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+            groups=1,
+            bias=True,
+            dilation=1,
+        )
         ffn_channel = FFN_Expand * c
-        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv4 = nn.Conv2d(
+            in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True
+        )
+        self.conv5 = nn.Conv2d(
+            in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True
+        )
 
         self.norm1 = LayerNorm2d(c)
         self.norm2 = LayerNorm2d(c)
@@ -131,15 +186,14 @@ class GCDB(nn.Module):
         self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
         self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
 
+    #        self.adapter = Adapter(c, ffn_channel=None)
 
-#        self.adapter = Adapter(c, ffn_channel=None)
-        
-#        self.use_adapters = False
+    #        self.use_adapters = False
 
-#    def set_use_adapters(self, use_adapters):
-#        self.use_adapters = use_adapters
-        
-    def forward(self, inp, adapter = None):
+    #    def set_use_adapters(self, use_adapters):
+    #        self.use_adapters = use_adapters
+
+    def forward(self, inp, adapter=None):
 
         y = inp
         x = self.norm1(inp)
@@ -148,17 +202,20 @@ class GCDB(nn.Module):
         z = 0
         for branch in self.branches:
             z += branch(x)
-        
+
         z = self.sg1(z)
         x = self.sca(z) * z
         x = self.conv3(x)
         y = inp + self.beta * x
-        #second step
-        x = self.conv4(self.norm2(y)) # size [B, 2*C, H, W]
+        # second step
+        x = self.conv4(self.norm2(y))  # size [B, 2*C, H, W]
         x = self.sg2(x)  # size [B, C, H, W]
-        x = self.conv5(x) # size [B, C, H, W]
+        x = self.conv5(x)  # size [B, C, H, W]
         x = y + x * self.gamma
+
+
 #################C3k2_GCDB######################################
+
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
     """Pad to 'same' shape outputs."""
@@ -167,8 +224,11 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
+
+
 class Conv(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
     default_act = nn.SiLU()  # default activation
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
@@ -185,6 +245,8 @@ class Conv(nn.Module):
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
         return self.act(self.conv(x))
+
+
 class Bottleneck_GCDB(nn.Module):
     """Standard bottleneck."""
 
@@ -202,6 +264,8 @@ class Bottleneck_GCDB(nn.Module):
     def forward(self, x):
         """'forward()' applies the YOLO FPN to input data."""
         return x + self.Attention(self.cv2(self.cv1(x))) if self.add else self.Attention(self.cv2(self.cv1(x)))
+
+
 class C2f_GCDB(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
@@ -213,7 +277,9 @@ class C2f_GCDB(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(Bottleneck_GCDB(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.m = nn.ModuleList(
+            Bottleneck_GCDB(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n)
+        )
 
     def forward(self, x):
         """Forward pass through C2f layer."""
@@ -227,6 +293,7 @@ class C2f_GCDB(nn.Module):
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
 
+
 class C3k(C3):
     """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
 
@@ -236,6 +303,7 @@ class C3k(C3):
         c_ = int(c2 * e)  # hidden channels
         self.m = nn.Sequential(*(Bottleneck_GCDB(c_, c_, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n)))
 
+
 class C3k2_GCDB(C2f_GCDB):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
@@ -243,5 +311,8 @@ class C3k2_GCDB(C2f_GCDB):
         """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
         super().__init__(c1, c2, n, shortcut, g, e)
         self.m = nn.ModuleList(
-            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck_GCDB(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n)
+            C3k(self.c, self.c, 2, shortcut, g)
+            if c3k
+            else Bottleneck_GCDB(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0)
+            for _ in range(n)
         )
